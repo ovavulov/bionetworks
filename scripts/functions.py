81d84c52e853f71b2b497eb64fe626e0d30b9040
ovavulov <ovavulov@mail.ru>
2021-03-25 11:52:02 +0300

    motif counting UPD +ecoli

--- scripts/functions.py
+++ scripts/functions.py
@@ -8,22 +8,38 @@ import os
 import json
 import joblib
 import numpy as np
-from itertools import permutations
+import pandas as pd
+from itertools import permutations, combinations, product
 from numba import njit, prange
 from tqdm import tqdm
-
-from subprocess import Popen, PIPE
-from datetime import datetime
+import networkx as nx
+import multiprocessing as mp
+from math import factorial
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
-def get_actual_parametrization(path, verbose=True):
+def read_ecoli_network(path):
+    f = open(path)
+    line = f.readline()
+    while line.startswith('#'):
+        line = f.readline()
+    df = pd.read_csv(f, sep="\t", header=None)
+    df.loc[-1] = line.split("\t")
+    df.index = df.index + 1
+    df = df.sort_index()
+    f.close()
+    return df
+
+
+def get_actual_parametrization(source, check_input=True, verbose=True):
         
-    cfg = json.load(open(path, "r"))
-    assert cfg["NETWORK_TO_SEARCH_IN"] in ["test", "yeast", "ecoli", "gs0.01", "gs0.1", "gs1"]
-    assert cfg["MOTIF_TO_SEARCH_FOR"] in ["feedforward", "fanout", "fanin", "cascade"]
+    cfg = source if type(source) is dict else json.load(open(source, "r"))
+    
+    if check_input:
+        assert cfg["NETWORK_TO_SEARCH_IN"] in ["ecoli", "test", "yeast", "ecoli", "gs0.01", "gs0.1", "gs1"]
+    
     if verbose:
         for param, value in cfg.items():
             print(f"{param}: {value}")
@@ -32,21 +48,12 @@ def get_actual_parametrization(path, verbose=True):
 
 def update_cfg(path, param, value, verbose=True):
     
-    cfg = get_actual_parametrization(path, verbose=False)
+    cfg = get_actual_parametrization(path, check_input=False, verbose=False)
     cfg[param] = value
+    cfg = get_actual_parametrization(cfg, verbose=verbose)
     json.dump(cfg, open(path, "w"))
-    cfg = get_actual_parametrization(path, verbose=verbose)
     
     return cfg
-
-
-def add_self_loops(interaction_matrix, indexes):
-    
-    selfloops = np.zeros(interaction_matrix.shape[0])
-    selfloops[indexes] = 1
-    selfloops = np.diag(selfloops)
-    
-    return interaction_matrix + selfloops
 
 
 def get_interacion_matrix(cfg):
@@ -54,33 +61,17 @@ def get_interacion_matrix(cfg):
     cwd = os.getcwd()    
     network = cfg["NETWORK_TO_SEARCH_IN"]
     interaction_matrix = joblib.load(
-        os.path.join(cwd, "networks", network, f"interaction_matrix.pkl")
+        os.path.join(cwd, "networks", network, f"interaction_matrix.gz")
     )
     
     return interaction_matrix
 
 
-def split_search_space(cfg, combs):
-    
-    idxs = np.linspace(0, len(combs), cfg["PARALLEL_THREADS_NUMBER"] + 1).astype(int)
-    combs_splitted = [combs[idxs[i]:idxs[i+1]] for i in range(cfg["PARALLEL_THREADS_NUMBER"])]
-    
-    splits_path = f"""./networks/{cfg["NETWORK_TO_SEARCH_IN"]}/splits"""
-
-    if not os.path.exists(splits_path):
-        os.mkdir(splits_path)
-        counter = 1
-        for combs_split in tqdm(combs_splitted):
-            joblib.dump(combs_split, os.path.join(splits_path, f"combs_{counter}.pkl"))
-            counter += 1
-    else:
-        print(f"Search space have been splitted into {len(os.listdir(splits_path))}")
-
 def build_motif_from_string(string):
     return np.array(list(map(int, string.split()))).reshape(3, 3)
 
+
 def get_equivalents(core_pattern):
-    
     pattern_variants = []
     for permutation in permutations(range(3)):
         variant = core_pattern[permutation, :]
@@ -90,12 +81,10 @@ def get_equivalents(core_pattern):
                 break
         else:
             pattern_variants.append(variant)
-    
     return pattern_variants
 
 
 def print_equivalents(cfg):
-    
     m = build_motif_from_string(json.load(open("./motifs_collection.json", "r"))[cfg["MOTIF_TO_SEARCH_FOR"]])
     if cfg["SELFLOOPS_INCLUDED"]: m += np.diag([1]*3)
     equivalents = get_equivalents(m)
@@ -104,69 +93,156 @@ def print_equivalents(cfg):
     for x in equivalents:
         print(x)
         print()
-    
+
+        
+def get_triad_codes():
+    motifs = json.load(open("./motifs_collection.json", "r"))
+    salt = np.array([2**i for i in range(6)])
+    mapping = {x: i for i, x in enumerate(motifs.keys())}
+    codes = {}
+    for motif in motifs.keys():
+        form = build_motif_from_string(motifs[motif])
+        isoforms = get_equivalents(form)
+        for isoform in isoforms:
+            mask = np.concatenate([np.diag(isoform, k=i) for i in [-2, -1, 1, 2]])
+            code =  mask @ np.array([2**i for i in range(6)])
+            codes[code] = mapping[motif]
+    xcodes = [-1 for _ in range(sum(salt)+1)]
+    for code, motif in codes.items():
+        xcodes[code] = motif
+    xcodes
+    return xcodes, {i: x for x, i in mapping.items()}
+
 
 @njit(cache=True)
-def get_motifs(pattern_variants, interaction_matrix, combs):
-    
-    genes = []
-    motifs = []
+def get_motifs(interaction_matrix, combs, codes, n):
+    triads = [[(-1, -1, -1)] for _ in range(n)]
+    salt = np.array([2**i for i in range(6)]).astype(np.float64)
     n_combinations = len(combs)
-    n_patterns = len(pattern_variants)
     for i in prange(n_combinations):
-        cl = np.array(combs[i])
+        c = combs[i]
+        cl = np.array(c)
         triad = interaction_matrix[cl, :][:, cl]
-        for j in prange(n_patterns):
-            pattern = pattern_variants[j]
-            if (triad - pattern == np.zeros(3)).all():
-                genes.append(cl)
-                motifs.append(triad)
-                break
-    
-    return genes, motifs
+        mask = [0]
+        for k in [-2, -1, 1, 2]:
+            mask += list(np.diag(triad, k=k))
+        mask = np.array(mask[1:]).astype(np.float64)
+        code = int(mask @ salt)
+        idx = codes[code]
+        if idx == -1:
+            pass
+        else:
+            triads[idx] += [c]
+    return triads
 
 
-def make_parallel_search(cfg):
+def motif_search(cfg, interaction_matrix, batch_size, verbose=False):
     
-    network = cfg["NETWORK_TO_SEARCH_IN"]
-    motif = cfg["MOTIF_TO_SEARCH_FOR"]
-    selfloops_included = cfg["SELFLOOPS_INCLUDED"]
-    results_path = f"./motif_search_results"
-    if not os.path.exists(results_path):
-        os.mkdir(results_path)
-    results_path = os.path.join(results_path, network)
-    if not os.path.exists(results_path):
-        os.mkdir(results_path)
-    results_path = os.path.join(results_path, motif + selfloops_included*"_sl")
-    if not os.path.exists(results_path):
-        os.mkdir(results_path)
+    network_name = cfg["NETWORK_TO_SEARCH_IN"]
+    codes, mapping = get_triad_codes()
+    N_CORES = mp.cpu_count() if cfg["N_CORES_TO_USE"] == -1 else cfg["N_CORES_TO_USE"]
+    
+    def connected_triads_generator(interaction_matrix):
+
+        interaction_matrix_adj = interaction_matrix - np.diag(np.diag(interaction_matrix))
+        tg_idxs, tf_idxs = np.where(interaction_matrix_adj != 0)
+        links = pd.DataFrame(index=range(len(tf_idxs)), columns=["tf", "tg"])
+        links.tf = tf_idxs
+        links.tg = tg_idxs
+        links_tf = links.set_index("tf", drop=False)[["tg"]]
+
+        cascades = links.join(links_tf[["tg"]], on="tg", how="inner", rsuffix="_final")
+        cascades = cascades[cascades.tf != cascades.tg_final]
+
+        for cascade in cascades.values:
+            yield tuple(cascade)
+
+        grouper = links.groupby("tg")
+        counter = grouper["tf"].count()
+        for tg in counter[counter > 1].index:
+            tf_pairs = combinations(links[links.tg == tg].tf.values, 2)
+            for tf_1, tf_2 in tf_pairs:
+                yield tf_1, tf_2, tg
+
+        grouper = links.groupby("tf")
+        counter = grouper["tg"].count()
+        for tf in counter[counter > 1].index:
+            tg_pairs = combinations(links[links.tf == tf].tg.values, 2)
+            for tg_1, tg_2 in tg_pairs:
+                yield tf, tg_1, tg_2
+    
+    triads = connected_triads_generator(interaction_matrix)
+    
+    def batch_generator(triads):
+        batch = []
+        counter = 0
+        for triad in triads:
+            batch.append(triad)
+            counter += 1
+            if counter == batch_size:
+                yield batch
+                batch = []
+                counter = 0
+        yield batch    
+    
+    def processor(splitted_triads):
         
-    start = datetime.now()
+        def gen_to_queue(input_q, splitted_triads):
+            for batch in splitted_triads:
+                input_q.put(batch)
+            for _ in range(N_CORES):
+                input_q.put(None)
 
-    N_PROCESSES = cfg["PARALLEL_THREADS_NUMBER"]
-    SCRIPT_PATH = "./scripts/find_motifs.py"
+        def process(input_q, output_q):
+            while True:
+                batch = input_q.get()
+                if batch is None:
+                    output_q.put(None)
+                    break
+                output_q.put(get_motifs(interaction_matrix, batch, codes, len(mapping)))
 
-    cmds_list = [["python", SCRIPT_PATH, str(i)] for i in range(1, N_PROCESSES + 1)]
-    procs_list = [Popen(cmd, stdout=PIPE, stderr=PIPE) for cmd in cmds_list]
-    for proc in procs_list:
-        proc.wait()
+        input_q = mp.Queue(maxsize = N_CORES * 2)
+        output_q = mp.Queue(maxsize = N_CORES * 2)
+
+        gen_pool = mp.Pool(1, initializer=gen_to_queue, initargs=(input_q, splitted_triads))
+        pool = mp.Pool(N_CORES, initializer=process, initargs=(input_q, output_q))
+
+        finished_workers = 0
+        while True:
+            result = output_q.get()
+            if result is None:
+                finished_workers += 1
+                if finished_workers == N_CORES:
+                    break
+            else:
+                yield result
+        
+        input_q = None
+        output_q = None
+        gen_pool.close()
+        gen_pool.join()
+        pool.close()
+        pool.join()
     
-    file_names = os.listdir(results_path)
-    genes_names = sorted([name for name in file_names if name.startswith("genes")])
-    motifs_names = sorted([name for name in file_names if name.startswith("motifs")])
+    
+    splitted_triads = batch_generator(triads)
+    motifs_generator = processor(splitted_triads)
 
-    genes = []
-    for genes_name in genes_names:
-        genes += joblib.load(os.path.join(results_path, genes_name))
-        os.remove(os.path.join(results_path, genes_name))
-    joblib.dump(genes, os.path.join(results_path, "genes.pkl"))
+    motifs = [[] for _ in range(len(mapping))]
+    for batch in tqdm(motifs_generator) if verbose else motifs_generator:
+        for i in range(len(mapping)):
+            if batch[i][1:] != []:
+                for triad in batch[i][1:]:
+                    motifs[i].append("_".join(map(str, sorted(triad))))
+    motifs = {mapping[i]: list(set(motifs[i])) for i in range(len(mapping))}
+    counter = {x: len(y) for x, y in motifs.items()}
+    
+    joblib.dump(motifs, f"./networks/{network_name}/motifs.gz")
+    json.dump(counter, open(f"./networks/{network_name}/counter.json", "w"))
+    
+    return motifs, counter
 
-    motifs = []
-    for motifs_name in motifs_names:
-        motifs += joblib.load(os.path.join(results_path, motifs_name))
-        os.remove(os.path.join(results_path, motifs_name))
-    joblib.dump(motifs, os.path.join(results_path, "motifs.pkl"))
-    print(f"""{len(motifs)} {motif}{" with selfloops" if selfloops_included else ""} motifs have been found""")
 
-    print(f"Total time spent: {datetime.now() - start}")
-
+def count_triads_nx(interaction_matrix):    
+    G = nx.DiGraph(interaction_matrix.T)
+    return nx.algorithms.triads.triadic_census(G)
